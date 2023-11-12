c313558c9bc641867374931cdd0bd45b65312c06
dimitri-edel <dimitri.edel@gmx.net>
2023-11-12 14:23:02 +0000

    Add pages.team.AddChatSidebar.js

--- src/pages/team/TeamChat.js
+++ src/pages/team/TeamChat.js
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 import styles from "../../styles/TeamChat.module.css";
 import { useCurrentUser } from "../../context/CurrentUserContext";
-import { Card, Modal, Button, Container, Row, Col, } from "react-bootstrap";
+import { Card, Modal, Button, Container, Row, Col, Nav, Navbar } from "react-bootstrap";
 import { useHistory, useParams } from "react-router-dom";
 import { axiosReq, axiosRes } from "../../api/axiosDefaults";
 import TeamMessageBoard from "./TeamMessageBoard";
 import TeamMessagePostForm from "./TeamMessagePostForm";
 import TeamChatFilters from "./TeamChatFilters";
+import TeamChatSidbar from "./TeamChatSidebar";
 import StaticContext from "../../context/StaticContext";
 
 const TeamChat = (props) => {
@@ -19,27 +20,38 @@ const TeamChat = (props) => {
     // have been added or removed from the board
     const [reload, setReload] = useState(false);
     // Value for search filter
-    const [searchFilter, setSearchFilter] = useState("")
+    const [searchFilter, setSearchFilter] = useState("");
     // Value for time filter (API expects a GET parameter &minus_days=? )
     // This filter returns messages that were posted in the past ? days
-    const [timeFilter, setTimeFilter] = useState(0)
+    const [timeFilter, setTimeFilter] = useState(0);
+
 
     return (
         <div className={styles.ChatRoom}>
             <h1 className={styles.TeamName}>{StaticContext.SELECTED_TEAM}</h1>
-            <TeamChatFilters
-                setSearchFilter={setSearchFilter}
-                setTimeFilter={setTimeFilter}
-                searchFilter={searchFilter}
-                timeFilter={timeFilter}
-            />
-            <TeamMessageBoard
-                team_id={team_id}
-                setReload={setReload}
-                reload={reload}
-                searchFilter={searchFilter}
-                timeFilter={timeFilter} />
-            <TeamMessagePostForm team_id={team_id} setReload={setReload} />
+            <Container>
+                <Row>
+                    <Col md={2}>
+                        <TeamChatSidbar team_id={team_id} />
+                    </Col>
+                    <Col>
+                        <TeamChatFilters
+                            setSearchFilter={setSearchFilter}
+                            setTimeFilter={setTimeFilter}
+                            searchFilter={searchFilter}
+                            timeFilter={timeFilter}
+                        />
+                        <TeamMessageBoard
+                            team_id={team_id}
+                            setReload={setReload}
+                            reload={reload}
+                            searchFilter={searchFilter}
+                            timeFilter={timeFilter} />
+                        <TeamMessagePostForm team_id={team_id} setReload={setReload} />
+                    </Col>
+                </Row>
+
+            </Container>
         </div>
     )
 }
---
+++ src/pages/team/TeamChatSidebar.js
@@ -0,0 +1,66 @@
+import React, { useState, useEffect } from "react";
+import styles from "../../styles/TeamChat.module.css";
+import { useCurrentUser } from "../../context/CurrentUserContext";
+import { Card, Modal, Button, Container, Row, Col, Nav, Navbar } from "react-bootstrap";
+import { useHistory, useParams } from "react-router-dom";
+import { axiosReq, axiosRes } from "../../api/axiosDefaults";
+
+const TeamChatSidbar = ({ team_id }) => {
+
+    // showUsers
+    const [expanded, setExpanded] = useState(false);
+    const [hasLoaded, setHasLoaded] = useState(false);
+    const userNameClick = (selected_key) => {
+        setExpanded(false);
+        alert(selected_key);
+    }
+
+    const [members, setMembers] = useState([]);
+
+    useEffect(() => {
+        /**
+     * Fetch all members of the team
+     */
+        const fetchMembers = async () => {
+            try {
+                // I want the Members to be ordered by their due date at all times
+                const { data } = await axiosReq.get(`/team-members/${team_id}`);
+                setMembers(data);
+                console.log(data);
+                // Signal that the content has been loaded
+                setHasLoaded(true);
+            } catch (err) {
+                console.log(err);
+            }
+        };
+
+        fetchMembers();
+
+    }, [team_id])
+
+    return (
+        <Navbar expanded={expanded} expand="md">
+            <Navbar.Toggle
+                onClick={() => setExpanded(!expanded)}
+                aria-controls="basic-navbar-nav"
+            >
+                <i className="fa-solid fa-user"></i>
+            </Navbar.Toggle>
+            <Navbar.Collapse>
+                <Nav onSelect={selectedKey => userNameClick(selectedKey)}>
+                    <Nav.Item>
+                        {hasLoaded ? (
+                            members.map(member => <Nav.Link key={member.user_id} eventKey={member.user_id}>{member.username} </Nav.Link>)
+
+                        ) :
+                            (<div>Loading ...</div>)
+                        }
+                    </Nav.Item>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    )
+
+}
+
+export default TeamChatSidbar;
